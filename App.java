da91e6e709f538352fc5df1d6e34389718df5cf3
DeianMarioRus <deianrus04@gmail.com>
2022-11-29 12:10:15 +0100

    Modificado

--- App.java
+++ App.java
@@ -1,9 +1,12 @@
 public class App {
   
   public static void main(String[] args) {
+    System.out.println("Deian Mario Rus");
     System.out.println("Contenido 2a");
     System.out.println("Contenido 3");
     System.out.println("Contenido 2b");
+    System.out.println("29/11/2022");
+    System.out.println("Resido en Leon");
   }
   
 }
